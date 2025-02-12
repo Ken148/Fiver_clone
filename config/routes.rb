97546ab755b839aa62f7dbcf0f61e3d7f2fe766c
Ken148 <ken.turk70@gmail.com>
2025-02-12 22:38:12 +0100

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "posts/new"
   devise_for :users
-  root 'posts#index' # Assuming posts#index is your main page
+  root 'home#index' # Assuming posts#index is your main page
 
   resources :posts, only: [:index, :new, :create, :show, :edit, :update]
 end
