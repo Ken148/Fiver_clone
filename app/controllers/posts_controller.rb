2d1144c41b46241af296638d13a717ecb8cecfe9
Ken148 <ken.turk70@gmail.com>
2025-02-12 22:10:29 +0100

    a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,4 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
-
-  def index
-    @posts = Post.all
-  end
-
-  def show
-    @post = Post.find(params[:id])
-  end
-
   def new
-    @post = current_user.posts.new
-  end
-
-  def create
-    @post = current_user.posts.new(post_params)
-    if @post.save
-      redirect_to @post, notice: 'Post was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "posts/new"
   devise_for :users
   root 'posts#index' # Assuming posts#index is your main page
 
