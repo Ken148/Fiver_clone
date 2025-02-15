e363376bf5bc3962cc97e6754e9c67ade3011f08
Ken148 <ken.turk70@gmail.com>
2025-02-15 20:11:05 +0100

    goggle login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get "posts/new"
-  devise_for :users
-  root 'home#index' # Assuming posts#index is your main page
+  
+  # Only one devise_for :users block
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
+  root 'home#index'  # Assuming home#index is your main page
 
   resources :posts, only: [:index, :new, :create, :show, :edit, :update]
 end
