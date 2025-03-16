39dd3662753ac3c518b29ef50b9842a73f24bb12
Ken148 <ken.turk70@gmail.com>
2025-03-16 11:10:49 +0100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-    I18n.locale = session[:locale] || I18n.default_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
